57c490554758f1212a14796ddd5765110166f5af
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-05-11 15:37:31 -0300

    commit inicial

---
+++ src/test/java/com/matheus/bff_agendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.bff_agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
